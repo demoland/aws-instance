0d5477d26951dfff603d53d24a5fae36d07c7a41
Daniel Fedick <daniel.fedick@hashicorp.com>
2024-01-25 12:43:02 -0500

    Fixup Ubuntu post-install

--- main.tf
+++ main.tf
@@ -48,18 +48,18 @@ resource "aws_instance" "generic_instance" {
 #!/bin/bash
 exec > /tmp/setup.log 2>&1
 
-# ### Install Docker #############################################################
-# sudo yum install -y jq
-# sudo yum install -y yum-utils
-# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-# sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-# ### Install Docker Compose #####################################################
-# sudo yum install -y python3-pip
-# sudo pip3 install docker-compose
-
-# ### Install AWS CLI ############################################################
-# sudo pip3 install awscli --upgrade
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 EOF
 
