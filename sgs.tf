520a3b64ae4597665088181fa75eedbe20e21fd4
Daniel Fedick <daniel.fedick@hashicorp.com>
2024-03-21 16:49:40 -0400

    Add sgs

--- sgs.tf
+++ sgs.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "ssh_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.cidr_block, var.my_ip]
+    cidr_blocks = [var.cidr_block, var.my_ip, "0.0.0.0/0"]
   }
 
   ingress {
@@ -16,7 +16,7 @@ resource "aws_security_group" "ssh_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [var.cidr_block, var.my_ip]
+    cidr_blocks = [var.cidr_block, var.my_ip, "0.0.0.0/0"]
   }
 
   ingress {
@@ -24,7 +24,7 @@ resource "aws_security_group" "ssh_sg" {
     from_port   = 8443
     to_port     = 8443
     protocol    = "tcp"
-    cidr_blocks = [var.cidr_block, var.my_ip]
+    cidr_blocks = [var.cidr_block, var.my_ip, "0.0.0.0/0"]
   }
 
   egress {
