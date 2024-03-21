0e627cc70d2e1865312f245d054569d959551a8b
Daniel Fedick <daniel.fedick@hashicorp.com>
2024-03-21 16:45:10 -0400

    Add sgs

--- main.tf
+++ main.tf
@@ -58,9 +58,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 EOF
 
 }
